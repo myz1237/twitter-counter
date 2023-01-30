0b63ae4e835f408ef412d8b21d0382c0da4ce6e4
myz1237 <mengyizhuoprivate@outlook.com>
2023-01-30 23:18:06 +0800

    update

--- index.js
+++ index.js
@@ -1,108 +1,17 @@
 
-import { getFollowers, getUserId } from "./utils.js";
+import { getFollowers, getUserFollowerCount, getUserId } from "./utils.js";
 import fs from 'fs'
 
+/*
+    1. Update the nameList
+    2. Run the code
+*/
+
 const nameList = [
-    // '1kxnetwork',
-	// 'RariCapital',
-	// '0xMaki',
-	// 'CryptoMessiah',
-	// 'Tetranode',
-	'bluekirbyfi',
-	'bantg',
-	'DeFianceCapital',
-	'Fiskantes',
-	'safetyth1rd',
-	'Daryllautk',
-	'EvgenyGaevoy',
-	'0mllwntrmt3',
-	'intangiblecoins',
-	'danrobinson',
-	'zhusu',
-	'richwgalvin',
-	'SBF_FTX',
-	'CryptoCobain',
-	'kyled116',
-	'Arthur_0x',
-	'IamNomad',
-	'CozomoMedici',
-	'convexdegen',
-	'KariyaKanav',
-	'danielesesta',
-	'josephdelong',
-	'CryptoSeq',
-	'dhof',
-	'richardchen39',
-	'kevinrose',
-	'Vince_Van_Dough',
-	'ptj_official',
-	'CryptoKaleo',
-	'ChrisBlec',
-	'Pentosh1',
-	'cmsintern',
-	'0xbunnygirl',
-	'crypto_bitlord7',
-	'0xSisyphus',
-	'WuBlockchain',
-	'CroissantEth',
-	'Lomashuk',
-	'AutomataEmily',
-	'matthuang',
-	'paradigm',
-	'machibigbrother',
-	'0xtuba',
-	'CathieDWood',
-	'mcuban',
-	'0x_b1',
-	'sama',
-	'wintermute_t',
-	'redphonecrypto',
-	'0xAkshay',
-	'ASvanevik',
-	'AlamedaTrabucco',
-	'chamath',
-	'cmsholdings',
-	'CurveFinance',
-	'TheChicagoVC',
-	'bigmagicdao',
-	'haydenzadams',
-	'lawmaster',
-	'jack',
-	'anildelphi',
-	'JayHao8',
-	'cdixon',
-	'YanLiberman',
-	'Dogetoshi',
-	'kaiynne',
-	'Shaughnessy119',
-	'draecomino',
-	'paoloardoino',
-	'iamDCinvestor',
-	'danheld',
-	'sassal0x',
-	'VentureCoinist',
-	'QwQiao',
-	'FEhrsam',
-	'el33th4xor',
-	'hasufl',
-	'iamjosephyoung',
-	'jbrukh',
-	'cburniske',
-	'PanteraCapital',
-	'RaoulGMI',
-	'gavofyork',
-	'DoveyWan',
-	'BarrySilbert',
-	'brian_armstrong',
-	'AriDavidPaul',
-	'novogratz',
-	'ethereumJoseph',
-	'ErikVoorhees',
-	'gavinandresen'
+    '1kxnetwork',
 ];
 
 async function main(){
-    const users = [];
     const title = '\uFEFFTarget Twitter,Follower Name,Follower UserName,Followers Count,Following Count,Tweet Count,Listed Count\r\n';
     fs.writeFileSync('result.csv', title, {flag: 'a'});
     for (const username of nameList){
@@ -112,19 +21,17 @@ async function main(){
             console.log('Cannot find ', username);
             continue;
         }
-        console.log('UserId: ', userId);
-        const data = await getFollowers(userId, username);
-        users.push(data)
+        const count = await getUserFollowerCount(userId);
+        if (!count) {
+            console.log('Cannot find followers.');
+            continue;
+        }
+        console.log(`${username} has ${count} followers, it will take ${Math.ceil(count / 1000)} mins to fetch all followers.`)
+        console.log('Fetching...');
+        await getFollowers(userId, username);
+        console.log(`Fetching followers of ${username} is done.`)
     }
 
-    // csvContent = users.reduce((pre, cur, index) => {
-    //     const userData = cur.reduce((previous, user) => {
-    //         return previous + `${nameList[index]},${user.username},${user.created_at},${user.location ?? 'Unknown'}\r\n`;
-    //     }, "");
-    //     return pre + userData
-    // }, "\uFEFFTarget Twitter,Follower UserName,Follower Account Created At,Follower Location\r\n");
-
-    // fs.writeFileSync('result.csv', csvContent, {flag: 'a'});
 }
 
 
--- utils.js
+++ utils.js
@@ -5,7 +5,6 @@ import fs from 'fs'
 const INTERVAL = 62 * 1000 // request per 62 secs
 
 export const getFollowers = async (userId, userName) => {
-    let users = [];
     let params = {
         "max_results": 1000,
         "user.fields": "public_metrics"
@@ -15,12 +14,9 @@ export const getFollowers = async (userId, userName) => {
     let hasNextPage = true;
     let nextToken = null;
     while (hasNextPage) {
-        
         let resp = await getPage(params, nextToken, url);
-        // console.log(resp)
         if (resp && resp.meta && resp.meta.result_count && resp.meta.result_count > 0) {
             if (resp.data) {
-                users.push.apply(users, resp.data);
                 writeFile(resp.data, userName);
             }
             if (resp.meta.next_token) {
@@ -33,9 +29,7 @@ export const getFollowers = async (userId, userName) => {
         }
         await sleep(INTERVAL);
     }
-    console.log(`${userName}: Got ${users.length} users.`);
     fs.writeFileSync('result.csv', `User Count,${users.length}\r\n`, {flag: 'a'});
-    return users;
 }
 
 function sleep (time) {
@@ -72,4 +66,20 @@ export const getUserId = async (username) => {
         return false
     }    
     
+}
+
+export const getUserFollowerCount = async (userId) => {
+    const url = `https://api.twitter.com/2/users/${userId}`;
+    const params = {
+       " user.fields": 'public_metrics'
+    }
+    try {
+        const result = await myAxios.get(url, {
+            params
+        });
+        return JSON.parse(result.data)?.data?.public_metrics?.followers_count
+    } catch (error) {
+        console.log(error?.message);
+        return false;
+    }   
 }
